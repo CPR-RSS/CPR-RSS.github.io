3622a9921c10ed04e4408af6c10c9e143db178ea
XgDuan <lazydxg@gmail.com>
2019-05-06 13:17:46 +0800

    Merge branch 'master' of https://github.com/conference-paper-rss/conference-paper-rss.github.io

--- paper_parser/iccv.py
+++ paper_parser/iccv.py
@@ -3,4 +3,5 @@ from paper_parser.cvpr import PaperListParser
 class PaperListParser(PaperListParser):
     def __init__(self, args):
         super().__init__(args)
+        assert args.year % 2 == 1, "ICCV only holds in the odd year"
         self.base_url = "http://openaccess.thecvf.com/ICCV%s.py" % (args.year)
