2465c906b1f97fa2d7bd77ba9828d234eed653ed
XgDuan <duan_xg@outlook.com>
2021-04-12 12:28:25 +0800

    Update: iclr2020

--- paper_parser/openreview.py
+++ paper_parser/openreview.py
@@ -15,8 +15,14 @@ from .base import BaseParser, Paper
 
 
 def read_page(url, delay=30, signature='note'):
-    print('reading page:{}; max timeout={}...'.format(url, delay))
-    browser = webdriver.Chrome()
+    if signature == 'note':
+        print('reading page:{}; max timeout={}...'.format(url, delay))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('blink-settings=imagesEnabled=false')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.get(url)
     try:
         _ = WebDriverWait(browser, delay).until(
@@ -61,8 +67,16 @@ class OpenreviewParser(BaseParser):
         for container in tqdm.tqdm(all_container, '' if url is None else url):
             try:
                 title = container.select('h4 > a')[0].get_text().strip()
-                url = container.select('a')[0].get('href')
-                paper_list.append((title, url))
+                url = container.select('h4 > a')[1].get('href')
+                # import pdb; pdb.set_trace()
+                author_list = [self.text_process(x.get_text())
+                               for x in container.select('div.note-authors > a')
+                               ]
+                abstract = [item.select_one('.note-content-value').get_text()
+                            for item in container.select('ul.note-content > li')
+                            if 'abstract' in item.select_one('.note-content-field').get_text().lower()
+                            ][0]
+                paper_list.append((title, abstract, url, author_list))
                 overall += 1
             except Exception as e:
                 print("Line (%d): %s" % (sys._getframe().f_lineno, str(e)))
@@ -73,19 +87,12 @@ class OpenreviewParser(BaseParser):
 
     def cook_paper(self, paper_info):
         try:
-            page_content = read_page(self.website_url + paper_info[1], signature='meta_row')
-            soup = BeautifulSoup(page_content, features="html.parser")
-            author_list = [item.get_text() for item in soup.select('span.signatures >  a')]
-            author_list = [self.text_process(x) for x in author_list]
-            abstract = [item.select_one('.note_content_value').get_text()
-                        for item in soup.select('div.note_contents')
-                        if 'abstract' in item.select_one('.note_content_field').get_text().lower()
-                        ][0]
-            abstract = self.text_process(abstract)
-            pdf_url = self.website_url + soup.select_one('a.note_content_pdf').get('href')
-            return Paper(self.text_process(paper_info[0]), abstract, pdf_url, author_list)
+            return Paper(self.text_process(paper_info[0]),
+                         self.text_process(paper_info[1]),
+                         self.website_url + paper_info[2],
+                         paper_info[3])
         except Exception as e:
             import traceback
             traceback.print_exc()
             print("Line (%d): %s" % (sys._getframe().f_lineno, str(e)))
-            return (paper_info[0], e, self.base_url, [])
+            return (paper_info[0], e, self.website_url + paper_info[1], [])
